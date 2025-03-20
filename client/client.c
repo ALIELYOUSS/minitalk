68d1a4e23bf8d4c50d5bbd3786780ae2d0162b94
Ali El Youssoufi <elyoussali25@gmail.com>
2025-03-20 00:06:59 +0000

    push

--- client/client.c
+++ client/client.c
@@ -1,52 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alel-you <alel-you@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 22:43:32 by alel-you          #+#    #+#             */
+/*   Updated: 2025/03/19 23:12:46 by alel-you         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minitalk.h"
 
-static void msg_converter(char *msg, int pid)
+static void	send_char(int pid, char c)
 {
-    char octet;
-    int i;
-    int x;
+	int	x;
 
-    i = 0;
-    octet = 0;
-    while (msg[i])
-    {
-        x = 128;
-        octet = msg[i];
-        while (x > 0)
-        {
-            if (octet >= x)
-            {
-                if (kill(pid, SIGUSR1) == -1)
-                    exit(0);
-                octet %= x;
-                x /= 2;
-            }
-            else
-            {
-                if (kill(pid, SIGUSR2))
-                    exit(0);
-                x/=2;
-            }
-            usleep(500);
-        }
-        i++;
-    }
-    ft_putstr_fd("msg sucssefully sent to: ", 1);
+	x = 128;
+	while (x > 0)
+	{
+		if (c >= x)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				exit(EXIT_FAILURE);
+			c %= x;
+			x /= 2;
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2))
+				exit(EXIT_FAILURE);
+			x /= 2;
+		}
+		usleep(500);
+	}
 }
 
-int main(int ac, char **av)
+static void	msg_converter(char *msg, int pid)
 {
-    int pid;
+	char	octet;
+	int		i;
 
-    if (ac != 3 || !ft_atoi(av[1]))
-        return (0);
-    pid = ft_atoi(av[1]);
-    if (pid == -1)
-        return (ft_putstr_fd("Error has occured\n", 1), exit(0), 0);
-    if (av[2])
-    {
-        msg_converter(av[2], pid);
-        ft_putnbr_fd(pid, 1);
-    }
-    return (0);
+	i = 0;
+	octet = 0;
+	while (msg[i])
+	{
+		octet = msg[i];
+		send_char(pid, octet);
+		octet = 0;
+		i++;
+	}
+	ft_putstr_fd("🥳msg succsfully sent to🥳: ", 1);
+}
+
+int	main(int ac, char **av)
+{
+	int	pid;
+
+	if (ac != 3 || !ft_atoi(av[1]))
+		return (0);
+	pid = ft_atoi(av[1]);
+	if (pid == -1)
+		return (ft_putstr_fd("Error has occured /-.-\\\n", 1), exit(0), 0);
+	if (av[2])
+		msg_converter(av[2], pid);
+	ft_putnbr_fd(pid, 1);
+	ft_putchar_fd('\n', 1);
+	return (0);
 }
